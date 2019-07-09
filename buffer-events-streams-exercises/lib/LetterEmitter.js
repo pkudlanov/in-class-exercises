bd9be63fece3887fac8dc615b5f3562724688b9b
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-09 16:41:08 -0700

    basic class set up and some instruction comments

---
+++ buffer-events-streams-exercises/lib/LetterEmitter.js
@@ -0,0 +1,17 @@
+const EventEmitter = require('events');
+
+class LetterEmitter extends EventEmitter {
+    //read method is like iterate method
+    //instead of array we'll get string
+    //need to split string to letters
+    //for each letter it needs to emit event
+    //event should include data
+    //'{ letter: 'b', offset: 20 }'
+    //when done emit end event
+    //event should probably be called letter
+    read(str) {
+        return str;
+    }
+}
+
+module.exports = LetterEmitter;
